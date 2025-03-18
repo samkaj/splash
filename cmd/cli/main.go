f5f5eb7c0a700037daf9b8bc303f397237fab14f
samkaj <kajavasamuel@gmail.com>
2025-03-18 21:50:44 +0100

    [cli] add flag parser and read input files

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,7 +1,75 @@
 package main
 
-import "fmt"
+import (
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
 
 func main() {
-    fmt.Println("hello from the cli :^)")
+	inputFilePath := flag.String("i", "", "")
+	flag.Usage = usage
+	flag.Parse()
+
+	_, err := readFile(*inputFilePath)
+	if err != nil {
+		fail(err.Error())
+	}
+
+	outputFormats := flag.Args()
+	if len(outputFormats) < 1 {
+		fail(ErrNoFormatsProvided.Error())
+	}
 }
+
+func readStdin() ([]byte, error) {
+	json, err := io.ReadAll(os.Stdin)
+	if err != nil {
+		return nil, ErrEmptyStdin
+	}
+
+	return json, nil
+}
+
+func readFile(path string) ([]byte, error) {
+	if path == "" {
+		return readStdin()
+	}
+
+	json, err := os.ReadFile(path)
+	if err != nil {
+		return nil, ErrFailedToReadFile(path)
+	}
+
+	return json, nil
+}
+
+func usage() {
+	fmt.Fprintf(os.Stderr, `splash exports base16 palettes to various formats
+
+USAGE: 
+    splash [OPTIONS] FORMAT [FORMAT ...]
+
+POSITIONAL ARGUMENTS:
+    <FORMAT>...  Output formats. Supported formats: []
+
+OPTIONS:
+    -i  JSON-file containing the palette. When omitted, stdin is used.
+    -h  Display this message.
+`)
+}
+
+func fail(message string) {
+	fmt.Fprintf(os.Stderr, "splash: %s. run with -h for usage.\n", message)
+	os.Exit(1)
+}
+
+func ErrFailedToReadFile(path string) error {
+	return fmt.Errorf("failed to read file '%s'", path)
+}
+
+var ErrEmptyStdin = errors.New("failed to read from stdin")
+
+var ErrNoFormatsProvided = errors.New("no formates provided")
